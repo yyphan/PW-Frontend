95ca35530fe79a24b89be471cd58a9349e57e822
Yao Yifan <jeromeyao1999@gmail.com>
2026-01-10 22:24:07 +0800

    chore: tweak css

--- src/components/ui/title-variants/ReaderTitle.tsx
+++ src/components/ui/title-variants/ReaderTitle.tsx
@@ -2,11 +2,11 @@ export default function ReaderTitle({ title }: { title: string }) {
     return (
         <div className="flex flex-wrap items-center gap-2 text-3xl font-medium">
             <span className="text-ide-func font-semibold">Read</span>
-            <span className="text-ide-fg">(</span>
+            <span className="text-ide-func font-semibold">(</span>
             <span className="font-sans font-bold text-2xl sm:text-3xl tracking-tight leading-none decoration-clone shadow-none">
                 "{title}"
             </span>
-            <span className="text-ide-fg">)</span>
+            <span className="text-ide-func font-semibold">)</span>
         </div>
     );
 }
